a41f32de768f82a21f72883faebaa50cc388c904
Anton Hakansson <anton.hakansson98@gmail.com>
2022-03-22 03:19:06 +0100

    repo: configure emacs projectile compilation

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . ((projectile-project-compilation-cmd . "doas nixos-rebuild switch --flake .")
+         (projectile-project-run-cmd . "doas nixos-rebuild test --flake .")
+         (projectile-project-test-cmd . "nix flake check")
+         )))
