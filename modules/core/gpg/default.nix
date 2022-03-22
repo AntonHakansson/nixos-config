a47b9ddc645a530a0135e3c2a8a5ae385e114f7b
Anton Hakansson <anton.hakansson98@gmail.com>
2022-03-22 02:31:55 +0100

    feat(gpg): tty pinentry for non-graphical system

--- modules/core/gpg/default.nix
+++ modules/core/gpg/default.nix
@@ -1,32 +1,34 @@
 { config, lib, pkgs, ... }:
 
 {
-  config = {
-    asdf.core.zfs.homeLinks = [{
-      path = ".gnupg";
-      type = "data";
-    }];
+  config =
+    let pinentryFlavor = if config.asdf.graphical.enable then "gtk2" else "tty";
+    in {
+      asdf.core.zfs.homeLinks = [{
+        path = ".gnupg";
+        type = "data";
+      }];
 
-    programs.gnupg.agent = {
-      enable = true;
-      pinentryFlavor = "gtk2";
-    };
-    home-manager.users.hakanssn = { lib, ... }: {
-      home.activation.fixPermissionsCommands =
-        lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-          chmod u=rwX,go= /home/hakanssn/.gnupg
-        '';
-      programs.gpg.enable = true;
-      services.gpg-agent = {
+      programs.gnupg.agent = {
         enable = true;
-        enableSshSupport = true;
-        defaultCacheTtl = 7200;
-        maxCacheTtl = 99999;
-        pinentryFlavor = "gtk2";
+        pinentryFlavor = pinentryFlavor;
+      };
+      home-manager.users.hakanssn = { lib, ... }: {
+        home.activation.fixPermissionsCommands =
+          lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+            chmod u=rwX,go= /home/hakanssn/.gnupg || true
+          '';
+        programs.gpg.enable = true;
+        services.gpg-agent = {
+          enable = true;
+          enableSshSupport = true;
+          defaultCacheTtl = 7200;
+          maxCacheTtl = 99999;
+          pinentryFlavor = pinentryFlavor;
 
-        # gpg --list-keys --with-keygrip
-        sshKeys = [ "789306B940E996B597D9D458942718C7E73B3A5F" ];
+          # gpg --list-keys --with-keygrip
+          sshKeys = [ "789306B940E996B597D9D458942718C7E73B3A5F" ];
+        };
       };
     };
-  };
 }
