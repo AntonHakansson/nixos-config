005ca0c9b4b3d023e97efcd6a420a585061bd68f
Anton Hakansson <anton.hakansson98@gmail.com>
2022-03-25 02:44:03 +0100

    feat(vim): convenient shellAliases

--- modules/core/vim/default.nix
+++ modules/core/vim/default.nix
@@ -1,8 +1,16 @@
-{ config, pkgs, ... }: {
-  config = {
-    programs.neovim = {
-      enable = true;
-      viAlias = true;
+{ config, pkgs, ... }:
+let
+  base = user: {
+    programs.zsh.shellAliases = {
+      v = "nvim";
+      vi = "nvim";
+      vim = "nvim";
     };
+  };
+in {
+  config = {
+    programs.neovim = { enable = true; };
+    home-manager.users.root = { ... }: (base "root");
+    home-manager.users.hakanssn = { ... }: (base "hakanssn");
   };
 }
