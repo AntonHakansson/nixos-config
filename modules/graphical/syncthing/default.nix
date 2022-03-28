abf357c9677b80c8c765a2756e32c716180334e3
Anton Hakansson <anton.hakansson98@gmail.com>
2022-03-28 19:05:11 +0200

    fix(syncthing): remove unused mount `~/sync`

--- modules/graphical/syncthing/default.nix
+++ modules/graphical/syncthing/default.nix
@@ -5,7 +5,6 @@
 
   config = lib.mkIf config.asdf.graphical.syncthing.enable {
     asdf.core.zfs.homeDataLinks = [ ".config/syncthing" ];
-    asdf.core.zfs.homeCacheLinks = [ "sync" ];
 
     home-manager.users.hakanssn = { pkgs, ... }: {
       services.syncthing.enable = true;
