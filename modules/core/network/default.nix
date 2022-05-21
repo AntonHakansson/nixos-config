134173411765da5f7968816967f996245999722d
Anton Hakansson <anton.hakansson98@gmail.com>
2022-05-21 16:40:50 +0200

    feat(wireless): new network

--- modules/core/network/default.nix
+++ modules/core/network/default.nix
@@ -5,7 +5,10 @@
     networking.wireless = {
       enable = true;
       environmentFile = config.age.secrets."passwords/networks.age".path;
-      networks = { "batman" = { psk = "@PSK_batman@"; }; };
+      networks = {
+        "batman" = { psk = "@PSK_batman@"; };
+        "TeliaGateway9C-97-26-91-52-01" = { psk = "@PSK_parents@"; };
+      };
     };
 
     age.secrets."passwords/networks.age" = {
