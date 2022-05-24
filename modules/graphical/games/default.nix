b8daa08b132dd0b29a2a4639e119e8637b6461f5
Anton Hakansson <anton.hakansson98@gmail.com>
2022-05-24 10:44:09 +0200

    fix(games): cache steam directory

--- modules/graphical/games/default.nix
+++ modules/graphical/games/default.nix
@@ -5,6 +5,7 @@
     lib.mkEnableOption "Enable game launchers";
 
   config = lib.mkIf config.asdf.graphical.games.enable {
+    asdf.core.zfs.homeCacheLinks = [ ".local/share/Steam" ];
     programs.steam.enable = true;
     asdf.core.nix.unfreePackages = [ "steam" "steam-original" "steam-runtime" ];
   };
