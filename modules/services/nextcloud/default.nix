927c1e43ccf335c2ff5b491a14b66fe26413ffe9
Anton Hakansson <anton.hakansson98@gmail.com>
2022-06-01 00:21:22 +0200

    chore(nextcloud): bump version 23->24

--- modules/services/nextcloud/default.nix
+++ modules/services/nextcloud/default.nix
@@ -24,7 +24,7 @@
         autoUpdateApps.enable = true;
         autoUpdateApps.startAt = "05:00:00";
 
-        package = pkgs.nextcloud23;
+        package = pkgs.nextcloud24;
         config = {
           # Further forces Nextcloud to use HTTPS
           overwriteProtocol = "https";
