6c8c2c79e737e237cd78afc30287e226de91319d
Anton Hakansson <anton.hakansson98@gmail.com>
2022-08-07 18:07:28 +0200

    chore(theme): format

--- modules/graphical/theme/modus-operandi.nix
+++ modules/graphical/theme/modus-operandi.nix
@@ -61,38 +61,6 @@ let
     fg-header = "#2a2a2a";
 
     bg-region = "#bcbcbc";
-
-    # Temporary
-    base00 = bg-main; # Default Background
-    base01 =
-      bg-dim; # Lighter Background (Used for status bars, line number and folding marks)
-    base02 = "#3e4451"; # Selection Background
-    base03 = "#545862"; # Comments, Invisibles, Line Highlighting
-    base04 = "#565c64"; # Dark Foreground (Used for status bars)
-    base05 = "#abb2bf"; # Default Foreground, Caret, Delimiters, Operators
-    base06 = "#b6bdca"; # Light Foreground (Not often used)
-    base07 = "#c8ccd4"; # Light Background (Not often used)
-    base08 =
-      "#e06c75"; # Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
-    base09 =
-      "#d19a66"; # Integers, Boolean, Constants, XML Attributes, Markup Link Url
-    base0A = "#e5c07b"; # Classes, Markup Bold, Search Text Background
-    base0B = "#98c379"; # Strings, Inherited Class, Markup Code, Diff Inserted
-    base0C =
-      "#56b6c2"; # Support, Regular Expressions, Escape Characters, Markup Quotes
-    base0D = "#61afef"; # Functions, Methods, Attribute IDs, Headings
-    base0E =
-      "#c678dd"; # Keywords, Storage, Selector, Markup Italic, Diff Changed
-    base0F = "#be5046"; # Deprecated
-    # base24 extension
-    base10 = "#21252b"; # Darker background
-    base11 = "#181a1f"; # Darkest background
-    base12 = "#ff616e"; # Bright Red
-    base13 = "#f0a45d"; # Bright Yellow
-    base14 = "#a5e075"; # Bright Green
-    base15 = "#4cd1e0"; # Bright Cyan
-    base16 = "#4dc4ff"; # Bright Blue
-    base17 = "#de73ff"; # Bright purple
   };
   iosevka = pkgs.iosevka-bin;
   iosevka-aile = pkgs.iosevka-bin.override { variant = "aile"; };
