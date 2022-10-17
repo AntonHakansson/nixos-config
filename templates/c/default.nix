552f86ac4483253e5e41ccd9693a1b537af03fd8
Anton Hakansson <anton.hakansson98@gmail.com>
2022-10-17 15:26:45 +0200

    feat(templates): c/c++ Makefile template

---
+++ templates/c/default.nix
@@ -0,0 +1,9 @@
+{ clangStdenv }:
+
+clangStdenv.mkDerivation rec {
+  pname = "foo-bar";
+  version = "0.1.0";
+
+  src = ./.;
+  makeFlags = [ "PREFIX=$(out)" ];
+}
--- templates/default.nix
+++ templates/default.nix
@@ -4,5 +4,10 @@ rec {
     description =
       "A flake-utils, devshell ready template to quickstart a new project";
   };
+  c-project = {
+    path = ./c;
+    description =
+      "A Makefile c/c++ template";
+  };
   default = hakanssn-project;
 }
