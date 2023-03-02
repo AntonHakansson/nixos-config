cd5e4cf06567391c78a9b2b24cc668a14691b2ff
Anton Hakansson <anton.hakansson98@gmail.com>
2023-03-02 12:06:34 +0100

    nit(dev): set `project.el` default compile command

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,8 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
 ((nil . ((projectile-project-compilation-cmd . "doas nixos-rebuild switch --flake .")
          (projectile-project-run-cmd . "doas nixos-rebuild test --flake . --fast")
          (projectile-project-test-cmd . "nix flake check")
-         )))
+         (compile-command . "doas nixos-rebuild test --flake .")))
+  )
