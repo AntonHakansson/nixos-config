8bbfe04c0775e4ce158e3f48648bd05296f2b76f
Anton Hakansson <anton.hakansson98@gmail.com>
2024-01-07 00:17:32 +0100

    bump

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,17 +5,17 @@
     # ...
     # });
 
-    river = prev.river.overrideAttrs (oldAttrs: rec {
-      pname = "river";
-      version = "git";
-      src = prev.fetchFromGitHub {
-        owner = "riverwm";
-        repo = pname;
-        rev = "c16628c7f57c51d50f2d10a96c265fb0afaddb02";
-        hash = "sha256-E3Xtv7JeCmafiNmpuS5VuLgh1TDAbibPtMo6A9Pz6EQ=";
-        fetchSubmodules = true;
-      };
-    });
+    # river = prev.river.overrideAttrs (oldAttrs: rec {
+    #   pname = "river";
+    #   version = "git";
+    #   src = prev.fetchFromGitHub {
+    #     owner = "riverwm";
+    #     repo = pname;
+    #     rev = "c16628c7f57c51d50f2d10a96c265fb0afaddb02";
+    #     hash = "sha256-E3Xtv7JeCmafiNmpuS5VuLgh1TDAbibPtMo6A9Pz6EQ=";
+    #     fetchSubmodules = true;
+    #   };
+    # });
   };
   stable-packages = final: prev: {
     stable = import inputs.nixpkgs-stable { system = final.system; };
