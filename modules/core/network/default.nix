3bbba45d3cdc72c252ea97cd11c91d41b392fb67
Anton Hakansson <anton.hakansson98@gmail.com>
2024-04-27 15:27:56 +0200

    fix(network): psk

--- modules/core/network/default.nix
+++ modules/core/network/default.nix
@@ -4,10 +4,12 @@
   config = lib.mkIf config.hakanssn.core.network.enable {
     networking.wireless = {
       enable = true;
+      userControlled.enable = true;
       environmentFile = config.age.secrets."passwords/networks.age".path;
       networks = {
         "batman" = { psk = "@PSK_batman@"; };
         "#Telia-523728" = { psk = "@PSK_parents@"; };
+        "#Telia-523728-2.4Ghz" = { psk = "@PSK_parents@"; };
       };
     };
 
