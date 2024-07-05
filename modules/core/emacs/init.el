c5befbca2b0068dbafc0927ec7061378e46debfb
Anton Hakansson <anton.hakansson98@gmail.com>
2024-07-05 08:12:27 +0200

    feat(emacs): elfeed: embark integration

--- modules/core/emacs/init.el
+++ modules/core/emacs/init.el
@@ -1550,8 +1550,28 @@ current buffer, killing it."
   :bind ("C-c o e" . elfeed)
   :custom
   (elfeed-sort-order 'ascending)
+  (elfeed-search-filter "@6-months-ago +unread +mustread")
+
   :config
-  (setq-default elfeed-search-filter "@6-months-ago +unread +mustread"))
+  (use-package embark
+    :preface
+    (defun embark-elfeed-target-url ()
+      "Target the URL of the elfeed entry at point."
+      (when-let (((derived-mode-p 'elfeed-search-mode))
+                 (entry (elfeed-search-selected :ignore-region))
+                 (url (elfeed-entry-link entry)))
+        `(url ,url ,(line-beginning-position) . ,(line-end-position))))
+
+    (defun embark-elfeed-url-candidates ()
+      "Target the URLs of the selected elfeed entries."
+      (when-let (((derived-mode-p 'elfeed-search-mode))
+                 (entries (elfeed-search-selected))
+                 (urls (mapcar #'elfeed-entry-link entries)))
+        (cons 'url urls)))
+    :config
+    (add-to-list 'embark-target-finders #'embark-elfeed-target-url)
+    (add-to-list 'embark-candidate-collectors #'embark-elfeed-url-candidates))
+  )
 
 (use-package elfeed-org
   ;; Load rss feeds from org file
