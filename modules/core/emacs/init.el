5b332a69055316dc3e2fce86aa13d8d5f171e305
Anton Hakansson <anton.hakansson98@gmail.com>
2024-07-30 13:22:14 +0200

    fix(emacs): notmuch: fcc-dirs

--- modules/core/emacs/init.el
+++ modules/core/emacs/init.el
@@ -1608,6 +1608,7 @@ current buffer, killing it."
   (message-send-mail-function 'message-send-mail-with-sendmail)
   (message-cite-reply-position 'below)
   (message-kill-buffer-on-exit t)
+  (notmuch-fcc-dirs nil)
 
   :custom
   (notmuch-show-logo nil)
