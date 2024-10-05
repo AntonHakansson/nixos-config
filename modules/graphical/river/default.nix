aaa9a95903b9e50d19ff79f282fbf1ab2ae57d9f
Anton Hakansson <anton.hakansson98@gmail.com>
2024-10-05 16:27:09 +0200

    fix(river): increase border width

--- modules/graphical/river/default.nix
+++ modules/graphical/river/default.nix
@@ -92,7 +92,7 @@
               riverctl map normal None XF86MonBrightnessDown spawn '${pkgs.brightnessctl}/bin/brightnessctl s -- -5%'
               riverctl map normal None XF86MonBrightnessUp   spawn '${pkgs.brightnessctl}/bin/brightnessctl s -- +5%'
 
-              riverctl border-width 4
+              riverctl border-width 8
               riverctl default-layout rivertile
               pkill rivertile; rivertile -view-padding 0 -outer-padding 0 &
 
