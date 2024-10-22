3593a4116d2d5fc3ffadcc5715e634f503c08942
Anton Hakansson <anton.hakansson98@gmail.com>
2024-10-22 12:22:27 +0200

    refactor(emacs): move `hk/fill-paragraph` to text-mode

--- modules/core/emacs/init.el
+++ modules/core/emacs/init.el
@@ -599,6 +599,31 @@
                 '(cape-dabbrev-min-3
                   cape-elisp-block
                   cape-file)))
+
+  (defun hk/fill-paragraph (&optional P)
+    "When called with prefix argument call `fill-paragraph'.
+Otherwise split the current paragraph into one sentence per line."
+    (interactive "P")
+    (if (not P)
+        (save-excursion
+          (let ((fill-column 12345678)) ;; relies on dynamic binding
+            (fill-paragraph) ;; this will not work correctly if the paragraph is
+            ;; longer than 12345678 characters (in which case the
+            ;; file must be at least 12MB long. This is unlikely.)
+            (let ((end (save-excursion
+                         (forward-paragraph 1)
+                         (backward-sentence)
+                         (point-marker))))  ;; remember where to stop
+              (beginning-of-line)
+              (while (progn (forward-sentence)
+                            (<= (point) (marker-position end)))
+                (just-one-space) ;; leaves only one space, point is after it
+                (delete-char -1) ;; delete the space
+                (newline)        ;; and insert a newline
+                ))))
+      ;; otherwise do ordinary fill paragraph
+      (fill-paragraph P)))
+
   :init
   (add-hook 'text-mode-hook 'hk/text-capf))
 
@@ -1707,30 +1732,6 @@ current buffer, killing it."
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
   (package-initialize)
   (setq use-package-always-ensure t))
-
-(defun hk/fill-paragraph (&optional P)
-  "When called with prefix argument call `fill-paragraph'.
-Otherwise split the current paragraph into one sentence per line."
-  (interactive "P")
-  (if (not P)
-      (save-excursion
-        (let ((fill-column 12345678)) ;; relies on dynamic binding
-          (fill-paragraph) ;; this will not work correctly if the paragraph is
-          ;; longer than 12345678 characters (in which case the
-          ;; file must be at least 12MB long. This is unlikely.)
-          (let ((end (save-excursion
-                       (forward-paragraph 1)
-                       (backward-sentence)
-                       (point-marker))))  ;; remember where to stop
-            (beginning-of-line)
-            (while (progn (forward-sentence)
-                          (<= (point) (marker-position end)))
-              (just-one-space) ;; leaves only one space, point is after it
-              (delete-char -1) ;; delete the space
-              (newline)        ;; and insert a newline
-              ))))
-    ;; otherwise do ordinary fill paragraph
-    (fill-paragraph P)))
 
 ;;;
 ;;; Meow remap to position-based keybindings instead of mnemonic keybindings
