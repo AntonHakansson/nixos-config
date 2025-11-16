82b4a6cb85c7887c42272a786ffa92ec22ed4222
Anton Hakansson <anton.hakansson98@gmail.com>
2025-11-16 11:58:53 +0100

    refactor(git): renamed options

--- modules/development/git/default.nix
+++ modules/development/git/default.nix
@@ -12,43 +12,35 @@
   };
 
   config =
-    let
-      base = {
+    lib.mkIf config.hakanssn.development.git.enable {
+      home-manager.users.hakanssn = { ... }: {
         home.packages = with pkgs; [ gitAndTools.gitflow git-crypt ];
         programs.git = {
           enable = true;
-
-          userEmail = config.hakanssn.development.git.email;
-          userName = "Anton Hakansson";
-
-          lfs.enable = true;
-
-          extraConfig = {
-            core = { whitespace = "trailing-space"; };
+          ignores = [ ".direnv" ".envrc" ];
+          settings = {
+            user.name = "Anton Hakansson";
+            user.email = config.hakanssn.development.git.email;
             github.user = "AntonHakansson";
-            safe.directory = "/home/hakanssn/repos/nixos-config";
 
-            # Aliases
+            lfs.enable = true;
+
+            core.whitespace = "trailing-space";
+
             url."https://github.com/".insteadOf = "gh:";
             url."git@github.com:AntonHakansson/".insteadOf = "ah:";
             url."https://gitlab.com/".insteadOf = "gl:";
             url."https://gist.github.com/".insteadOf = "gist:";
             url."https://bitbucket.org/".insteadOf = "bb:";
+
+            aliases = {
+              # Reset
+              unadd = "reset HEAD";
+            };
+
+            safe.directory = "/home/hakanssn/repos/nixos-config";
           };
-          aliases = {
-            # Reset
-            unadd = "reset HEAD";
-          };
-          ignores = [ ".direnv" ".envrc" ];
-          # signing = {
-          #   key = "anton@hakanssn.com";
-          #   signByDefault = config.hakanssn.graphical.enable;
-          # };
         };
       };
-    in
-    lib.mkIf config.hakanssn.development.git.enable {
-      home-manager.users.hakanssn = { ... }: base;
-      home-manager.users.root = { ... }: base;
     };
 }
